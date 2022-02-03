1e93d5b6dce4571beb66dca4c21a9eb437a3537f
mbierma <fertilesound@yahoo.com>
2022-02-02 18:16:49 -0600

    Create interfaces for services

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/services/VetService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic2.services;
+
+import guru.springframework.sfgpetclinic2.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
