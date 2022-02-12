a26922dd43db23cb309fbda1fe260cc8cbbd5a15
mbierma <fertilesound@yahoo.com>
2022-02-12 09:41:44 -0600

    Refactored services to a CrudService interface

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/services/PetService.java
@@ -2,12 +2,6 @@ package guru.springframework.sfgpetclinic2.services;
 
 import guru.springframework.sfgpetclinic2.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/services/VetService.java
@@ -2,12 +2,6 @@ package guru.springframework.sfgpetclinic2.services;
 
 import guru.springframework.sfgpetclinic2.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
