4ce77a8988406d73260bd4b4e891c0e250153d8f
mbierma <fertilesound@yahoo.com>
2022-02-12 10:25:55 -0600

    Added index page controller

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic2/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfgpetclinic2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        //Thymeleaf looks for index template and returns it here
+        return "index";
+    }
+}
