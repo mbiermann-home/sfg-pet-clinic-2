5fd84e1756496713479331dabf40bc7f70ba785d
mbierma <fertilesound@yahoo.com>
2022-02-21 11:41:24 -0600

    added specialties map impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic2.services;
+
+import guru.springframework.sfgpetclinic2.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+}
