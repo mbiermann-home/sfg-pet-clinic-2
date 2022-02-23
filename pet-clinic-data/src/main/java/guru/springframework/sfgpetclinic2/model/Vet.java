b3f9a13c1b0d5ca5538e40d61328e8f6f6276beb
mbierma <fertilesound@yahoo.com>
2022-02-23 17:03:51 -0600

    Updated vet with JPA entity mappings

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic2/model/Vet.java
@@ -1,10 +1,17 @@
 package guru.springframework.sfgpetclinic2.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    // FetchType.EAGER ensures the specialties are loaded with vet, otherwise would be null
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+                inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
